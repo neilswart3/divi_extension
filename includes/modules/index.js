bba76cfd7471757508a11289f69eb0e76841907b
Neil Swart <neil@webstreet.co.za>
2019-09-03 22:13:02 +0200

    text header complete

--- includes/modules/index.js
+++ includes/modules/index.js
@@ -1,9 +1,5 @@
-import ContactDetails from './ContactDetails/ContactDetails';
-import ContactDetailsItem from './ContactDetailsItem/ContactDetailsItem';
 import TextHeader from './TextHeader/TextHeader';
 
 export default [
-    ContactDetails,
-    ContactDetailsItem,
-    TextHeader,
+    TextHeader
 ];
